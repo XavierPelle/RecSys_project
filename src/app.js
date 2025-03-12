993046a6f8008a178d5aaf1c87387407b8ff5e14
nolan25 <favandnolan@gmail.com>
2025-03-12 14:50:18 +0100

    hh

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { client } = require("./config/database.js");
-const { processCsvFiles } = require("./data/insertData");
+
+const app = express();
+
+const port = 1051;
 
 const { router } = require("./route.js");
 
@@ -11,6 +14,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/suggest", router);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs"); // Ne pas mettre .ejs, Express le détecte automatiquement
+});
 
 client
   .connect()
@@ -40,7 +47,6 @@ client
   .catch((err) => {
     console.error("Error connecting to PostgreSQL database", err);
   });
-processCsvFiles();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/data/insertData.js
+++ src/data/insertData.js
@@ -5,86 +5,90 @@ const fastcsv = require("fast-csv");
 const batchSize = 5000;
 
 function processCsvFiles() {
-    const pool = new Pool({
-        user: 'main',
-        password: 'FB1atQfXCPptY7I7BuMsAX91bGcTXx4R',
-        host: 'recsys_db',
-        port: '5432',
-        database: 'recsys_db',
-    });
+  const pool = new Pool({
+    user: "main",
+    password: "FB1atQfXCPptY7I7BuMsAX91bGcTXx4R",
+    host: "recsys_db",
+    port: "5432",
+    database: "recsys_db",
+  });
 
-    const createTableQueries = [
-        `
+  const createTableQueries = [
+    `
       CREATE TABLE IF NOT EXISTS train_sessions (
         session_id INTEGER,
         item_id INTEGER
       );
     `,
-        `
+    `
       CREATE TABLE IF NOT EXISTS train_purchases (
         session_id INTEGER,
         item_id INTEGER
       );
-    `
-    ];
+    `,
+  ];
 
-    pool.connect((err, client, done) => {
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.error("Erreur de connexion à la base de données:", err.stack);
+      return;
+    }
+
+    createTableQueries.forEach((query) => {
+      client.query(query, (err, res) => {
         if (err) {
-            console.error("Erreur de connexion à la base de données:", err.stack);
-            return;
+          console.error("Erreur lors de la création des tables:", err.stack);
         }
-
-        createTableQueries.forEach(query => {
-            client.query(query, (err, res) => {
-                if (err) {
-                    console.error("Erreur lors de la création des tables:", err.stack);
-                }
-            });
-        });
-
-        processCsvFile(client, "src/data/train_sessions.csv", "train_sessions");
-        processCsvFile(client, "src/data/train_purchases.csv", "train_purchases");
+      });
     });
 
-    pool.on('error', (err) => {
-        console.error('Erreur de pool PostgreSQL:', err.stack);
-    });
+    processCsvFile(client, "src/data/train_sessions.csv", "train_sessions");
+    processCsvFile(client, "src/data/train_purchases.csv", "train_purchases");
+  });
+
+  pool.on("error", (err) => {
+    console.error("Erreur de pool PostgreSQL:", err.stack);
+  });
 }
 
 function processCsvFile(client, fileName, tableName) {
-    let stream = fs.createReadStream(fileName);
-    let csvData = [];
+  let stream = fs.createReadStream(fileName);
+  let csvData = [];
 
-    let csvStream = fastcsv
-        .parse()
-        .on("data", function (data) {
-            csvData.push(data);
-        })
-        .on("end", function () {
-            csvData.shift();
+  let csvStream = fastcsv
+    .parse()
+    .on("data", function (data) {
+      csvData.push(data);
+    })
+    .on("end", function () {
+      csvData.shift();
 
-            let batch = [];
-            csvData.forEach((row, index) => {
-                batch.push(row);
+      let batch = [];
+      csvData.forEach((row, index) => {
+        batch.push(row);
 
-                if (batch.length === batchSize || index === csvData.length - 1) {
-                    let insertQuery = `INSERT INTO ${tableName} (session_id, item_id) VALUES ` +
-                        batch.map((_, i) => `($${2 * i + 1}, $${2 * i + 2})`).join(", ");
+        if (batch.length === batchSize || index === csvData.length - 1) {
+          let insertQuery =
+            `INSERT INTO ${tableName} (session_id, item_id) VALUES ` +
+            batch.map((_, i) => `($${2 * i + 1}, $${2 * i + 2})`).join(", ");
 
-                    client.query(insertQuery, batch.flat(), (err, res) => {
-                        if (err) {
-                            console.error(`Erreur d'insertion par lot dans ${tableName}:`, err.stack);
-                        } else {
-                            console.log(`Inséré ${res.rowCount} ligne(s) dans ${tableName}.`);
-                        }
-                    });
+          client.query(insertQuery, batch.flat(), (err, res) => {
+            if (err) {
+              console.error(
+                `Erreur d'insertion par lot dans ${tableName}:`,
+                err.stack
+              );
+            } else {
+              console.log(`Inséré ${res.rowCount} ligne(s) dans ${tableName}.`);
+            }
+          });
 
-                    batch = [];
-                }
-            });
-        });
+          batch = [];
+        }
+      });
+    });
 
-    stream.pipe(csvStream);
+  stream.pipe(csvStream);
 }
 
 module.exports = { processCsvFiles };
