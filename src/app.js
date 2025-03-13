2e41a453f7a224c25c51854dd550da63843a7a15
XavierPelle <xavierp34970@gmail.com>
2025-03-13 10:14:30 +0000

    fix client connect

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { client } = require("./config/database.js");
+const path = require('path');
 const { processCsvFiles } = require('./data/insertData')
 
 const app = express();
-
 const port = 1051;
-
 const { router } = require("./route.js");
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+processCsvFiles()
+
+client.connect();
 
 app.use(express.json());
 app.use(cors());
 
-processCsvFiles();
-
 app.use("/suggest", router);
 
 app.get("/", (req, res) => {
-  res.render("index.ejs"); // Ne pas mettre .ejs, Express le détecte automatiquement
+  res.render("index");
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
